41dc9d32cf89c3fc658ef53165b808aa15fd8091
Scrom <formain337@gmail.com>
2025-07-06 23:08:06 +0530

    otp validator added

---
+++ backend/src/controllers/OtpValidator.Controller.ts
@@ -0,0 +1,26 @@
+import { RequestHandler, Response } from "express";
+import { CustomReq } from "../interfaces/CustomReq.Interface";
+import { Otp } from "../models/Otp.Model";
+import { verifyHashedData } from "../utilities/Hasher.Utility";
+
+export const otpValidator: RequestHandler = async (req: CustomReq, res: Response) => {
+    try {
+        const { otp, email } = req.body;
+        const otpData = await Otp.findOne({ email });
+        if( !otpData || !otpData.otp ){
+            res.json({ message:"There is no user with this email", success: false })
+            return
+        }
+        const validateOtp = await verifyHashedData( otp, otpData.otp )
+        if( !validateOtp ){
+            res.json({ message:"Otp you entered is wrong.", success: false })
+            return
+        }
+        res.json({ message: "Otp Verified!", success: true })
+        return;
+    } catch (error) {
+        console.log(error)
+        res.json({ message:"Internal Server Error", success: false })
+        return;
+    }
+}
