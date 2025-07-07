259abc169a86aa19b13eaa608108dbaaaecd535a
Scrom <formain337@gmail.com>
2025-07-07 16:02:08 +0530

    added redux persist

--- frontend/src/app/store.tsx
+++ frontend/src/app/store.tsx
@@ -1,17 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import authReducer from '../features/auth/authSlice'
 import userReducer from '../features/user/userSlice'
 import errMsgReducer from "../features/errorMsgs/errMsgsSlice"
 import userContactReducer from '../features/userContact/userContactSlice'
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
+const rootReducer = combineReducers({
+  auth: authReducer,
     user: userReducer,
     errMsgs: errMsgReducer,
     userContact : userContactReducer
-  },
 })
+
+const persistConfig = {
+  key : "root",
+  storage,
+  whitelist: ["user"]
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer
+})
+
+export const persistor = persistStore(store)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
