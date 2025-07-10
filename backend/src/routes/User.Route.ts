46736db3db80f4759f5405e0e6fc4c93e410b1a4
Scrom <formain337@gmail.com>
2025-07-10 20:52:36 +0530

    routes updated

--- backend/src/routes/User.Route.ts
+++ backend/src/routes/User.Route.ts
@@ -6,12 +6,16 @@ import { updateUsername } from "../controllers/updateUsername.Controller";
 import { isLoggedIn } from "../middlewares/isLoggedIn";
 import { otpValidator } from "../controllers/OtpValidator.Controller";
 import { errMsgsFetch } from "../controllers/ErrMsgsFetch.Controller";
+import { getErrMsg } from "../controllers/GetErrMsg.Controller";
+import { checkLoggedIn } from "../controllers/checkLoggedIn.Controller";
 
 export const userRouter = Router();
 
+userRouter.get("/isLoggedIn", isLoggedIn, checkLoggedIn)
 userRouter.post("/signup", signup)
 userRouter.post("/otpValidator", otpValidator)
 userRouter.post("/signin", signin)
 userRouter.post("/updateUsername", isLoggedIn, updateUsername)
 userRouter.post("/contactInfoUpdate", updateContactInfo)
-userRouter.post("/errMsgsFetch", isLoggedIn, errMsgsFetch)
+userRouter.get("/errMsgsFetch", isLoggedIn, errMsgsFetch)
+userRouter.get("/getErrMsg/:errMsgID", isLoggedIn, getErrMsg)
