9df9c4d8fd94978bd4b94227afe04ebdf5a11e1d
Scrom <formain337@gmail.com>
2025-07-10 20:08:32 +0530

    dashboard page completed

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,38 +2,41 @@ import { useDispatch, useSelector } from "react-redux"
 import NavigationBar from "../components/NavigationBar"
 import { selectUser } from "../features/user/userSlice"
 import { selectErrMsgs, setErrMsgs } from "../features/errorMsgs/errMsgsSlice"
-import type { ErrMsgInterface, ErrMsgPropsInterface } from "../interfaces/ErrMsg.Interface"
+import type { ErrMsg } from "../interfaces/ErrMsg.Interface"
 import { useEffect } from "react"
 import { errMsgFetch } from "../requestHandler/ErrMsgFetch.ReqHandler"
+import { useNavigate } from "react-router-dom"
 
 
 
 const Dashboard = () => {
   const dispatch = useDispatch();
-  const username:string = useSelector(selectUser)
-  const errMsgs: ErrMsgInterface[] = useSelector(selectErrMsgs)
 
-  useEffect(()=>{
-    const fetchErrMsgs = async() => {
+  const username: string = useSelector(selectUser)
+  const errMsgs = useSelector(selectErrMsgs)
+
+  useEffect(() => {
+    const fetchErrMsgs = async () => {
       const errMsgs = await errMsgFetch();
-      if(Array.isArray(errMsgs)){
+      console.log(errMsgs)
+      if (Array.isArray(errMsgs)) {
         dispatch(setErrMsgs(errMsgs))
       }
-      else{
+      else {
         console.error("Error fetching error messages", errMsgs)
       }
     }
     fetchErrMsgs()
-  },[])
+  }, [])
 
   return (
     <div className="h-full w-full">
       <NavigationBar position="static" />
       <div className="h-[calc(100%-100px)] w-full relative">
-        <div className="h-full w-full absolute right-0">
-          <h1 className="font-mainheading font-extrabold bg-gradient-to-b from-white to-[#9A9A9A] bg-clip-text text-transparent text-4xl pl-20">{username + "'s"+ " DASHBOARD"}</h1>
+        <div className="h-full w-full">
+          <h1 className="font-mainheading font-extrabold bg-gradient-to-b from-white to-[#9A9A9A] bg-clip-text text-transparent text-4xl ml-30">{username + "'s" + " DASHBOARD"}</h1>
           <div className="h-[calc(100%-100px)] w-full overflow-y-scroll mt-10">
-            {errMsgs.length!==0 && errMsgs.map((errMsg)=>errMsg.solved === true? <SolvedError errName={errMsg.errName} time={errMsg.time} key={errMsg.id}/> : <UnsolvedError errName={errMsg.errName} time={errMsg.time} key={errMsg.id}/>)}
+          {errMsgs.length !== 0 && errMsgs.map((errMsg) => ( <ErrorCard key={errMsg._id} errMsg={errMsg}  /> ))}
           </div>
         </div>
       </div>
@@ -41,28 +44,31 @@ const Dashboard = () => {
   )
 }
 
-const SolvedError = ({errName, time}:ErrMsgPropsInterface) => {
-  return (
-    <div className="h-[130px] w-auto border border-secondary mb-20 mx-20 rounded-2xl overflow-hidden bg-third relative mt-5">
-      <div className="h-[52px] w-[712px] bg-secondary rounded-full blur-[200px] top-0 left-1/2 transform -translate-x-1/2 absolute"></div>
-      <div className="h-full ml-15 flex flex-col justify-center gap-3">
-        <h2 className="font-roboto font-extrabold text-[#CACACA]">{errName}</h2>
-        <h2 className="font-roboto text-[#CACACA] text-[13px]">{time}</h2>
-      </div>
-    </div>
-  )
-}
+const ErrorCard = ({ errMsg }: { errMsg: ErrMsg }) => {
+  const navigate = useNavigate();
 
-const UnsolvedError = ({errName, time}:ErrMsgPropsInterface) => {
+  const date = new Date(errMsg.createdAt);
+  const localDate = date.toLocaleString();
+
+  const borderColor = errMsg.solved ? 'border-secondary' : 'border-[#FF0000]';
+  const bgColor = errMsg.solved ? 'bg-third' : 'bg-[#310000]';
+  const glowColor = errMsg.solved ? 'bg-secondary' : 'bg-[#FF0000]';
+  const textColor = errMsg.solved ? 'text-[#CACACA]' : 'text-[#FF0000]';
+
+  const handleOnClick = (errMsgID: string) => {
+    navigate(`/errDetails/${errMsgID}`)  
+  }
+
   return (
-    <div className="h-[130px] w-auto border border-[#FF0000] mb-20 mx-20 rounded-2xl overflow-hidden bg-[#310000] relative mt-5">
-      <div className="h-[52px] w-[712px] bg-[#FF0000] rounded-full blur-[200px] top-0 left-1/2 transform -translate-x-1/2 absolute"></div>
-      <div className="h-full ml-15 flex flex-col justify-center gap-3 text-[#FF0000]">
-        <h2 className="font-roboto font-extrabold ">{errName}</h2>
-        <h2 className="font-roboto text-[13px]">{time}</h2>
+    <div className={`h-[130px] w-auto border ${borderColor} mb-20 mx-30 rounded-2xl overflow-hidden ${bgColor} relative mt-5`} onClick={() => {handleOnClick(String(errMsg._id))}}>
+      <div className={`h-[52px] w-[712px] ${glowColor} rounded-full blur-[200px] top-0 left-1/2 transform -translate-x-1/2 absolute`}></div>
+      <div className={`h-full ml-15 flex flex-col justify-center gap-3 ${textColor}`}>
+        <h2 className="font-roboto font-extrabold">{errMsg.errMsgObj.errName}</h2>
+        <h2 className="font-roboto text-[13px]">{localDate}</h2>
       </div>
     </div>
-  )
-}
+  );
+};
+
 
 export default Dashboard
--- frontend/src/requestHandler/ErrMsgFetch.ReqHandler.ts
+++ frontend/src/requestHandler/ErrMsgFetch.ReqHandler.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
-import type { ErrMsgInterface } from "../interfaces/ErrMsg.Interface"
+import type { ErrMsg } from "../interfaces/ErrMsg.Interface"
 
-export const errMsgFetch = async():(Promise<ErrMsgInterface[]|string>) => {
+export const errMsgFetch = async():(Promise<ErrMsg[]|string>) => {
     try {
-        const res = await axios.get("http://localhost:3000/user/errMsgFetch")
-        if(res.data.success) return res.data.errMsgs as ErrMsgInterface[]
+        const res = await axios.get("http://localhost:3000/user/errMsgsFetch", {
+            withCredentials: true
+        })
+        if(res.data.success) return res.data.errMsgs as ErrMsg[]
         else return res.data.message as string
     } catch (error) {
         return "There was an error fetching the err messages, please try reload the page."
