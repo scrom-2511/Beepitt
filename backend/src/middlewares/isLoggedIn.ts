78b843ee3c4e3625fbf645af064938f87bc5f88c
Scrom <formain337@gmail.com>
2025-07-10 20:53:47 +0530

    app.tsx index.css main.tsx added to git

--- backend/src/middlewares/isLoggedIn.ts
+++ backend/src/middlewares/isLoggedIn.ts
@@ -1,32 +1,30 @@
-import { NextFunction, Response } from "express";
+import { NextFunction, RequestHandler, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { CustomReq } from "../interfaces/CustomReq.Interface";
 
 const jwtSecret = process.env.JWT_SECRET;
 
-export const isLoggedIn = (req: CustomReq, res: Response, next: NextFunction) => {
-    console.log("Camer htere")
-    console.log(req.cookies)
-    if (!jwtSecret) {
-        console.error("JWT_SECRET not set");
-        res.status(500).json({ message: "Server error", success: false });
-        return;
-    }
-
-    // const token = req.cookies.token;
-    // console.log(token)
-
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySUQiOiI2ODZjZDg1NWU2MzE2OGUxN2E2NTM5Y2IiLCJpYXQiOjE3NTE5NzMxMjZ9.NPeM1E2p4mRmhlXJNhzFJYSlQskVgqo7EbUfAeEMsSc"
-
-    if (!token) {
-        console.log("Token required")
-        res.status(401).json({ message: "Token required", success: false });
-        return;
-    }
-
+export const isLoggedIn: RequestHandler = (req: CustomReq, res: Response, next: NextFunction) => {
     try {
+        console.log("Camer htere")
+        console.log(req.cookies)
+        if (!jwtSecret) {
+            console.error("JWT_SECRET not set");
+            res.status(500).json({ message: "Server error", success: false });
+            return;
+        }
+
+        const { token } = req.cookies;
+        console.log("Token is ", token)
+
+        if ( !token ) {
+            console.log("Token required")
+            res.status(401).json({ message: "Token required", success: false });
+            return;
+        }
+        
         const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
-        req.body.userID = decoded.userID
+        req.userID = decoded.userID
         next();
     } catch (err) {
         res.status(401).json({ message: "Invalid or expired token", success: false });
