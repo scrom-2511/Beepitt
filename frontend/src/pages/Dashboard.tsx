7e50eb91037d0430982293d2264aaee247af3959
scrom <formain337@gmail.com>
2026-01-13 13:52:41 +0000

    fix: top bar fixed in dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,7 +5,9 @@ const Dashboard = () => {
   return (
     <SidebarProvider className="h-full w-full p-5">
       <AppSidebar />
-      <TopBar />
+      <div className="bg-background w-full h-full rounded-2xl">
+        <TopBar />
+      </div>
     </SidebarProvider>
   );
 };
@@ -14,13 +16,12 @@ export default Dashboard;
 
 const TopBar = () => {
   return (
-    <div className="bg-background w-full h-full rounded-2xl">
+    <>
       <div className="flex p-5 gap-5 items-center font-montserrat">
         <SidebarTrigger />
         <h1 className="text-foreground font-medium text-xl">Dashboard</h1>
       </div>
       <div className="border border-foreground opacity-25"></div>
-    </div>
+    </>
   );
 };
-
