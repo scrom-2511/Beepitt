9eb67c8640bf05914231df6bd614f3aeef27d18b
scrom <formain337@gmail.com>
2026-01-13 13:45:29 +0000

    add: top bar in dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,11 +5,22 @@ const Dashboard = () => {
   return (
     <SidebarProvider className="h-full w-full p-5">
       <AppSidebar />
-      <div className="bg-background w-full h-full rounded-2xl">
-        <SidebarTrigger />
-      </div>
+      <TopBar />
     </SidebarProvider>
   );
 };
 
 export default Dashboard;
+
+const TopBar = () => {
+  return (
+    <div className="bg-background w-full h-full rounded-2xl">
+      <div className="flex p-5 gap-5 items-center font-montserrat">
+        <SidebarTrigger />
+        <h1 className="text-foreground font-medium text-xl">Dashboard</h1>
+      </div>
+      <div className="border border-foreground opacity-25"></div>
+    </div>
+  );
+};
+
