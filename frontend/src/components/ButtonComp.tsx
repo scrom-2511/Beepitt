0856c7a758d41e385372a68a05a0bd77791166f4
scrom <formain337@gmail.com>
2026-01-13 12:55:59 +0000

    add: onclick prop

---
+++ frontend/src/components/ButtonComp.tsx
@@ -0,0 +1,32 @@
+import type { VariantProps } from "class-variance-authority";
+import { motion } from "framer-motion";
+import { Button, buttonVariants } from "./ui/button";
+
+const ButtonComp = ({
+  children,
+  className = "flex-1 w-full py-6 font-semibold cursor-pointer",
+  variant = "default",
+  size = "default",
+  asChild = false,
+  onClick, // optional
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+    onClick?: React.MouseEventHandler<HTMLButtonElement>; // optional
+  }) => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.03 }}
+      whileTap={{ scale: 0.97 }}
+      transition={{ type: "spring", stiffness: 100 }}
+      className="w-full"
+    >
+      <Button size={size} variant={variant} className={className} onClick={onClick}>
+        {children}
+      </Button>
+    </motion.div>
+  );
+};
+
+export default ButtonComp;
