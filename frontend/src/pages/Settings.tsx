9943de2a72a145af80f98019dd8bb1175ce0b398
scrom <formain337@gmail.com>
2026-01-14 08:03:39 +0000

    add: profile settings ui

--- frontend/src/pages/Settings.tsx
+++ frontend/src/pages/Settings.tsx
@@ -1,4 +1,7 @@
+import { CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Label } from "@radix-ui/react-label";
 
 const tabItems = [
   { label: "Profile", value: "profile" },
@@ -38,3 +41,72 @@ const Settings = () => {
 };
 
 export default Settings;
+
+const Profile = () => {
+  return (
+    <section>
+      <div className="w-full h-40 rounded-2xl grid grid-cols-[400px_auto]">
+        <div className="flex flex-col h-full w-full p-10">
+          <h1 className="text-foreground text-xl mb-2">Profile</h1>
+          <p className="text-muted-foreground text-sm">
+            Set your account details
+          </p>
+        </div>
+        <div>
+          <CardContent className="p-10">
+            <form>
+              <div className="flex flex-col gap-6 text-foreground text-sm">
+                {/* First and Last Name */}
+                <div className="flex w-full gap-5">
+                  <div className="grid gap-2 flex-1">
+                    <Label htmlFor="firstName">First Name</Label>
+                    <Input
+                      id="firstName"
+                      type="text"
+                      placeholder="First Name"
+                      required
+                      className="py-6"
+                    />
+                  </div>
+
+                  <div className="grid gap-2 flex-1">
+                    <Label htmlFor="lastName">Last Name</Label>
+                    <Input
+                      id="lastName"
+                      type="text"
+                      placeholder="Last Name"
+                      required
+                      className="py-6"
+                    />
+                  </div>
+                </div>
+
+                <div className="grid gap-2">
+                  <Label htmlFor="username">Email</Label>
+                  <Input
+                    id="username"
+                    type="text"
+                    placeholder="Username"
+                    required
+                    className="py-6"
+                  />
+                </div>
+
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                    className="py-6"
+                  />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+        </div>
+      </div>
+    </section>
+  );
+};
