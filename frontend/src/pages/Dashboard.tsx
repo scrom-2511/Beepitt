86b854d258e46659697cc2e37f7d4c8dfa580065
scrom <formain337@gmail.com>
2026-01-14 10:35:05 +0000

    update: now the topbar shows the page name acc to the selected tab in the sidebar

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -5,6 +5,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { UnseenIncidents } from "@/components/UnseenIncidents";
 import { useState } from "react";
+import Settings from "./Settings";
 
 const Dashboard = () => {
   const [selected, setSelected] = useState<string>("Open Incidents");
@@ -12,10 +13,11 @@ const Dashboard = () => {
     <SidebarProvider className="h-full w-full p-5">
       <AppSidebar selected={selected} setSelected={setSelected} />
       <ScrollArea className="bg-background w-full h-full rounded-2xl pb-5">
-        <TopBar />
+        <TopBar selected={selected} />
         {selected === "Unseen Incidents" && <UnseenIncidents />}
         {selected === "Open Incidents" && <OpenIncidents />}
         {selected === "Closed Incidents" && <ClosedIncidents />}
+        {selected === "Settings" && <Settings />}
       </ScrollArea>
     </SidebarProvider>
   );
@@ -23,12 +25,12 @@ const Dashboard = () => {
 
 export default Dashboard;
 
-const TopBar = () => {
+const TopBar = ({ selected }: { selected: string }) => {
   return (
     <>
       <div className="flex p-5 gap-5 items-center font-montserrat">
         <SidebarTrigger />
-        <h1 className="text-foreground font-medium text-xl">Dashboard</h1>
+        <h1 className="text-foreground font-medium text-xl">{selected}</h1>
       </div>
       <div className="border border-foreground opacity-25"></div>
     </>
