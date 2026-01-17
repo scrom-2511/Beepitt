1aa89faab63bfc185314d4c740e6953bad5358e1
scrom <formain337@gmail.com>
2026-01-17 11:50:36 +0000

    add: is logged is middleware

--- backend/src/middlewares/isLoggedIn.ts
+++ backend/src/middlewares/isLoggedIn.ts
@@ -1,37 +1,40 @@
-import { NextFunction, RequestHandler, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
-import { CustomReq } from "../interfaces/CustomReq.Interface";
+import { ERROR_CODES, HttpStatus } from "../types/errorCodes";
 
 const jwtSecret = process.env.JWT_SECRET;
 
-export const isLoggedIn: RequestHandler = (req: CustomReq, res: Response, next: NextFunction) => {
-    try {
-        // console.log("Camer htere")
-        // console.log(req.cookies)
-        if (!jwtSecret) {
-            console.error("JWT_SECRET not set");
-            res.status(500).json({ message: "Server error", success: false });
-            return;
-        }
+export interface CustomReq extends Request {
+  userId: number;
+}
 
-        const { token } = req.cookies;
-        // console.log("Token is ", token)
+export const isLoggedIn = (
+  req: CustomReq,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (!jwtSecret) {
+    console.error("JWT_SECRET not set");
+    return res.status(500).json({ message: "Server error", success: false });
+  }
 
-        if ( !token ) {
-            console.log("Token required")
-            res.status(401).json({ message: "Token required", success: false });
-            return;
-        }
-        
-        const decoded = jwt.verify(token, jwtSecret) as JwtPayload;
-        if (!decoded || typeof decoded !== 'object' || !decoded.userID) {
-            res.status(401).json({ message: "Invalid token payload", success: false });
-            return;
-        }
-        req.userID = decoded.userID
-        next();
-    } catch (err) {
-        res.status(401).json({ message: "Invalid or expired token", success: false });
-        return;
-    }
+  const { authToken } = req.cookies;
+
+  if (!authToken) {
+    return res.status(HttpStatus.UNAUTHORIZED).json({
+      success: false,
+      error: ERROR_CODES.UNAUTHORIZED,
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(authToken, jwtSecret) as JwtPayload;
+    req.userId = decoded.id;
+    next();
+  } catch (err) {
+    return res.status(HttpStatus.UNAUTHORIZED).json({
+      success: false,
+      error: ERROR_CODES.UNAUTHORIZED,
+    });
+  }
 };
