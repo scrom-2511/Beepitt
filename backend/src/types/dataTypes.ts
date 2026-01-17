dd5bf67e76138f2d1fb00ea02bcd742f43779aa0
scrom <formain337@gmail.com>
2026-01-17 12:14:16 +0000

    add: new datatypes

--- backend/src/types/dataTypes.ts
+++ backend/src/types/dataTypes.ts
@@ -10,3 +10,42 @@ export const SignupType = z.object({
   username: z.string().email(),
   password: z.string(),
 });
+
+export const onErrorFromClientType = z.object({
+  userId: z.number(),
+  errorName: z.string(),
+  errorDesc: z.string(),
+  jwtToken: z.string(),
+});
+
+export interface ProducerMessage {
+  key: string;
+  value: string;
+}
+
+export const ProfileUpdateType = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+});
+
+export const DateFormat = z.enum([
+  "YYYY-MM-DD",
+  "MM/DD/YYYY",
+  "DD/MM/YYYY",
+  "YYYY/MM/DD",
+  "MM-DD-YYYY",
+  "DD-MM-YYYY",
+]);
+
+export const TimeZoneAndPreferencesUpdateType = z.object({
+  city: z.string(),
+  timeZone: z.string().refine((tz) => {
+    try {
+      Intl.DateTimeFormat(undefined, { timeZone: tz });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }),
+  dateFormat: DateFormat,
+});
