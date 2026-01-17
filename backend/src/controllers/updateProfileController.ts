bf39470a52c9f13c83e8eb80fc8e569a06d68285
scrom <formain337@gmail.com>
2026-01-17 11:49:59 +0000

    add: update profile controller

---
+++ backend/src/controllers/updateProfileController.ts
@@ -0,0 +1,43 @@
+import { Response } from "express";
+import { prisma } from "../database/prismaClient";
+import { CustomReq } from "../middlewares/isLoggedIn";
+import { ProfileUpdateType } from "../types/dataTypes";
+import { ERROR_CODES, HttpStatus } from "../types/errorCodes";
+
+export const updateProfileController = async (
+  req: CustomReq,
+  res: Response,
+) => {
+  try {
+    const validateData = ProfileUpdateType.safeParse(req.body);
+    if (!validateData.success) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        success: false,
+        error: {
+          code: ERROR_CODES.INVALID_INPUT.code,
+          message: ERROR_CODES.INVALID_INPUT.message,
+        },
+      });
+    }
+
+    const user = await prisma.user.update({
+      where: { id: req.userId },
+      data: {
+        firstName: validateData.data.firstName,
+        lastName: validateData.data.lastName,
+      },
+    });
+
+    return res.status(HttpStatus.OK).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: {
+        code: ERROR_CODES.INTERNAL_SERVER_ERROR.code,
+        message: ERROR_CODES.INTERNAL_SERVER_ERROR.message,
+      },
+    });
+  }
+};
