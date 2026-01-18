1b8d70ea51393bbcff9fd863d5eaaa33312e7784
scrom <formain337@gmail.com>
2026-01-18 19:07:09 +0000

    delete: unnecesary handlers

--- frontend/src/requestHandler/Signin.ReqHandler.ts
+++ frontend/src/requestHandler/Signin.ReqHandler.ts
@@ -1,20 +1,58 @@
-import axios from "axios"
-import type { ResponseInterface } from "../interfaces/Response.Interfaces";
+import axios from "axios";
 
-export interface SigninInterface extends ResponseInterface{
-    username:string;
+export interface SigninRequest {
+  email: string;
+  password: string;
 }
 
-export const signinHandler = async (data: object): Promise<SigninInterface | ResponseInterface> => {
-    try {
-        const res = await axios.post("http://localhost:3000/user/signin", data, {
-            withCredentials: true
-        });
-        if (res.data.success) {
-            return { success: true, message: res.data.message, username: res.data.username }
-        }
-        return { success: false, message: res.data.message }
-    } catch (err) {
-        return { success: false, message: "There was an error, please try again.",  }
+type SigninResponse =
+  | { success: true }
+  | {
+      success: false;
+      data: {
+        error: {
+          id: number;
+          code: string;
+          message: string;
+        };
+      };
+    };
+
+export const signinHandler = async (
+  data: SigninRequest,
+): Promise<SigninResponse> => {
+  try {
+    const res = await axios.post("http://localhost:3000/user/signin", data, {
+      withCredentials: true,
+    });
+
+    if (res.data.success) {
+      return { success: true };
     }
-}
+
+    return {
+      success: false,
+      data: {
+        error: res.data.error,
+      },
+    };
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const data = err.response?.data;
+      if (data && !data.success) {
+        return { success: false, data };
+      }
+    }
+
+    return {
+      success: false,
+      data: {
+        error: {
+          id: 0,
+          code: "UNKNOWN",
+          message: "There was an error, please try again.",
+        },
+      },
+    };
+  }
+};
--- frontend/src/requestHandler/Signup.ReqHandler.ts
+++ frontend/src/requestHandler/Signup.ReqHandler.ts
@@ -1,14 +1,59 @@
-import axios from "axios"
-import type { ResponseInterface } from "../interfaces/Response.Interfaces";
+import axios from "axios";
 
-export const signupHandler = async (data: object): Promise<ResponseInterface> => {
-    try {
-        const res = await axios.post("http://localhost:3000/user/signup", data);
-        if (res.data.success) {
-            return { success: true, message: res.data.message }
-        }
-        return { success: false, message: res.data.messages }
-    } catch (err) {
-        return { success: false, message: "There was an error, please try again." }
-    }
+export interface SignupRequest {
+  email: string;
+  username: string;
+  password: string;
 }
+
+type SignupResponse =
+  | { success: true }
+  | {
+      success: false;
+      data: {
+        error: {
+          id: number;
+          code: string;
+          message: string;
+        };
+      };
+    };
+
+export const signupHandler = async (
+  data: SignupRequest,
+): Promise<SignupResponse> => {
+  try {
+    const res = await axios.post("http://localhost:3000/user/signup", data, {
+      withCredentials: true,
+    });
+
+    if (res.data.success) {
+      return { success: true };
+    }
+
+    return {
+      success: false,
+      data: {
+        error: res.data.error,
+      },
+    };
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const data = err.response?.data;
+      if (data && !data.success) {
+        return { success: false, data };
+      }
+    }
+
+    return {
+      success: false,
+      data: {
+        error: {
+          id: 0,
+          code: "UNKNOWN",
+          message: "There was an error, please try again.",
+        },
+      },
+    };
+  }
+};
