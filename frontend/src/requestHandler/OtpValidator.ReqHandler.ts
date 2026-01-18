5b32e0fe3440a424864b0ae99b68ea1a2bb2763b
scrom <formain337@gmail.com>
2026-01-18 19:04:00 +0000

    add: otpValidator req handler

--- frontend/src/requestHandler/OtpValidator.ReqHandler.ts
+++ frontend/src/requestHandler/OtpValidator.ReqHandler.ts
@@ -1,13 +1,61 @@
 import axios from "axios";
 
-export const otpValidator = async(data:object) => {
-    try {
-        const res = await axios.post("http://localhost:3000/user/otpValidator",data);
-    if (res.data.success) {
-        return {success:true, message:res.data.message}
-    }
-    return {success:false, message:res.data.message}
-    } catch (error) {
-    return {success:false, message:error}
-    }
+export interface otpValidatorRequest {
+  otp: string;
 }
+
+type otpValidatorResponse =
+  | { success: true }
+  | {
+      success: false;
+      data: {
+        error: {
+          id: number;
+          code: string;
+          message: string;
+        };
+      };
+    };
+
+export const otpValidatorHandler = async (
+  data: otpValidatorRequest,
+): Promise<otpValidatorResponse> => {
+  try {
+    const res = await axios.post(
+      "http://localhost:3000/user/otpValidator",
+      data,
+      {
+        withCredentials: true,
+      },
+    );
+
+    if (res.data.success) {
+      return { success: true };
+    }
+
+    return {
+      success: false,
+      data: {
+        error: res.data.error,
+      },
+    };
+  } catch (err) {
+    if (axios.isAxiosError(err)) {
+      const data = err.response?.data;
+      if (data && !data.success) {
+        return { success: false, data };
+      }
+    }
+
+    return {
+      success: false,
+      data: {
+        error: {
+          id: 0,
+          code: "UNKNOWN",
+          message: "There was an error, please try again.",
+        },
+      },
+    };
+  }
+};
