f30bcdf053ad07816926d4f5b5dcd105fb1b7217
scrom <formain337@gmail.com>
2026-01-19 11:40:01 +0000

    add: pricing page

--- frontend/src/pages/Pricing.tsx
+++ frontend/src/pages/Pricing.tsx
@@ -1,36 +1,88 @@
-import NavigationBar from "../components/NavigationBar"
+import ButtonComp from "@/components/ButtonComp";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Check } from "lucide-react";
+
+interface FeaturesItems {
+  featureItem: string;
+}
+interface PriceCardItems {
+  title: string;
+  price: string;
+  features: FeaturesItems[];
+}
+
+const items: PriceCardItems[] = [
+  {
+    title: "Basic Plan",
+    price: "Free",
+    features: [
+      { featureItem: "Validity - 3 Days" },
+      { featureItem: "Upto 1 member" },
+      {
+        featureItem:
+          "Notification Channels: Discord, Telegram (will be adding more soon)",
+      },
+    ],
+  },
+  {
+    title: "Premium Plan",
+    price: "$5",
+    features: [
+      { featureItem: "Validity - 30 Days" },
+      { featureItem: "Upto 4 members" },
+      {
+        featureItem:
+          "Notification Channels: Discord, Telegram (will be adding more soon)",
+      },
+    ],
+  },
+];
 
 const Pricing = () => {
   return (
-    <div className="h-full w-full">
-      <NavigationBar position="relative"/>
-      <h1 className="font-mainheading font-extrabold bg-gradient-to-b from-white to-[#9A9A9A] bg-clip-text text-transparent text-3xl text-center mt-24">ONE PRICE, ZERO HASSLE</h1>
-      <div className="flex justify-center gap-20 mt-24">
-        <CardComponent price="10" duration="month" />
-        <CardComponent price="100" duration="year" />
+    <div className="h-full w-full bg-background grid grid-rows-[300px_auto]">
+      <div className="flex h-full w-full justify-center items-end">
+        <h1 className="font-montserrat text-foreground text-5xl font-medium">
+          One price. That's all you need.
+        </h1>
+      </div>
+      <div className="h-full w-full flex p-30 gap-8 justify-center">
+        {items.map((item) => (
+          <Card key={item.title} className="bg-card flex-1 p-10 justify-center">
+            <CardHeader className="p-0">
+              <CardTitle className="font-light mb-3 text-muted-foreground">
+                {item.title}
+              </CardTitle>
+              <CardTitle className="text-5xl font-light">
+                {item.price}
+              </CardTitle>
+            </CardHeader>
+            <Separator />
+            <CardContent className="py-4 px-0">
+              <div className="flex flex-col gap-3">
+                {item.features.map((feature) => (
+                  <div className="flex flex-row text-lg text-muted-foreground items-center gap-2">
+                    <Check />
+                    {feature.featureItem}
+                  </div>
+                ))}
+              </div>
+            </CardContent>
+            <CardFooter>
+              <ButtonComp variant={"default"}>Create an account</ButtonComp>
+            </CardFooter>
+          </Card>
+        ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-const CardComponent = ({ price, duration }: { price: string, duration: string }) => {
-  return (
-    <div className="h-[450px] w-[300px] bg-third rounded-2xl relative">
-      <div className="h-[70px] w-[70px] bg-secondary rounded-full blur-[90px] absolute top-20 left-1/2 transform -translate-x-1/2"></div>
-      <div className="p-10 text-secondary">
-        <div>
-        <span className="font-roboto-flex font-extrabold text-4xl">${price}</span>
-        <span className="font-roboto-flex text-sm">/{duration}</span>
-        </div>
-        <div className="mt-15">
-          <p className="font-roboto text-sm mb-5">✔ Get instant mail on backend err.</p>
-          <p className="font-roboto text-sm mb-5">✔ Get instant call on backend err.</p>
-          <p className="font-roboto text-sm mb-5">✔ No hidden charges.</p>
-        </div>
-        <button className="h-7 w-30 bg-gradient-to-b from-white to-[#9A9A9A] rounded-[7px] font-roboto font-extrabold text-[10px] main-btn text-third absolute bottom-10 left-1/2 transform -translate-x-1/2">ENABLE ALERTS</button>
-      </div>
-    </div>
-  )
-}
-
-export default Pricing
+export default Pricing;
