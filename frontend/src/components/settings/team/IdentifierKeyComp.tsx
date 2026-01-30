7cc33fe5c931f29291af83c85c50cd84caf5f54e
scrom <formain337@gmail.com>
2026-01-30 05:35:41 +0000

    add: identifier key component

---
+++ frontend/src/components/settings/team/IdentifierKeyComp.tsx
@@ -0,0 +1,26 @@
+import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
+
+const IdentifierKeyComp = () => {
+  return (
+    <div className="flex flex-col gap-6 text-muted-foreground text-sm">
+      <div className="flex w-full gap-5">
+        <div className=" flex-1">
+          <Label htmlFor="identifier key">Identifier Key</Label>
+          <div
+            onMouseDown={async () => {
+              await navigator.clipboard.writeText("identifier key");
+              toast.success("Copied to clipboard");
+            }}
+            id="identifierKey"
+            className="py-4 px-6 sm:py-6 text-foreground placeholder:text-xs sm:placeholder:text-sm mt-2 bg-input/30 rounded-2xl"
+          >
+            inkajdfslk
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdentifierKeyComp;
