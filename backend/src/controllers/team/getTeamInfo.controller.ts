6367b4b34609ffe4d8cfc18c44f860987ee6511c
scrom <formain337@gmail.com>
2026-01-30 06:42:18 +0000

    add: get team info controller

---
+++ backend/src/controllers/team/getTeamInfo.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from 'express';
+import { prisma } from '../../database/prismaClient';
+import { ERROR_CODES, HttpStatus } from '../../types/errorCodes';
+
+export const getTeamInfoController = async (req: Request, res: Response) => {
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      res.status(HttpStatus.UNAUTHORIZED).json({
+        success: false,
+        error: {
+          id: ERROR_CODES.UNAUTHORIZED.id,
+          code: ERROR_CODES.UNAUTHORIZED.code,
+          message: ERROR_CODES.UNAUTHORIZED.message,
+        },
+      });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    const chatIds = await prisma.contactDetails.findUnique({
+      where: { userId },
+    });
+
+    res.status(HttpStatus.OK).json({
+      success: true,
+      data: {
+        identifierKey: user?.identifierKey,
+        telegramChatIds: chatIds?.telegramChatIds,
+        discordChatIds: chatIds?.discordChatIds,
+      },
+    });
+    return;
+  } catch (error) {
+    console.error(error);
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      error: {
+        id: ERROR_CODES.INTERNAL_SERVER_ERROR.id,
+        code: ERROR_CODES.INTERNAL_SERVER_ERROR.code,
+        message: ERROR_CODES.INTERNAL_SERVER_ERROR.message,
+      },
+    });
+    return;
+  }
+};
